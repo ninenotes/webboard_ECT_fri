cf5043002b2428ba0666926f179474bb2cf44f31
ninenotes <111536344+ninenotes@users.noreply.github.com>
2024-03-08 12:21:22 +0700

    work 10 not finish
    work 10 not finish

--- post.php
+++ post.php
@@ -4,45 +4,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Post</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
-    <?php 
+    <!-- <?php
     $id = $_GET['id'];
-    ?>
-<h1 style="text-align: center;">Webboard KakKak</h1>
-    <Hr>
-    <div style="text-align: center;">
-    <?php echo"ต้องการดูกระทู้หมายเลข $id";
-      echo"<BR>";
+    ?> -->
+<div class="container-lg">
+     <h1 style="text-align: center;" class="mt-3">Webboard KakKak</h1>
+    <?php include "nav.php" ?>
+    <div class="row mt-4">
+     <div class="col-1g-3 col-md-2 col-sm-1"></div>
+    <div class="col-1g-6 col-md-8 col-sm-10">
     
-    if ($id %2==0) {
-        echo"เป็นกระทู้หมายเลขคู่";
+    <?php
+    $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+    $sql="SELECT post.title,post.content,post.post_date,user.login FROM post INNER JOIN user ON(post.user_id=user.id) WHERE post.id=$_GET[id]";
+    $result=$conn->query($sql);
+    while($row=$result->fetch()){
+
+     echo"   <div class='card boder-primary'>";
+     echo"    <div class='card-header bg-primary text-white'>$row[0]</div>";
+     echo"<div class='card-body'>$row[1]</div>";
+ 
+        
+        
+    echo"</div>";
     }
-    if ($id %2==1) {
-        echo"เป็นกระทู้หมายเลขคี่";
-    }
-    
+
     ?>
-
+    <div class="card border-success mt-3">
+        <div class="card-header bg-success text-white">
+            แสดงความคิดเห็น</div>
+    <div class="card-body">
+        <form action="post_save.php" method="post">
+            <input type="hidden" name="post_id"
+            value="<?= $_GET['id'];?>">
+            <div class="row mb-3 justify-content-center">
+                <div class="col-1g-10">
+                    <textarea name="comment" rows="8"
+                    class="form-control" required></textarea>
+                </div>
+            </div>
+            <div class="row">
+    <div class="col-1g-12 d-flex justify-content-center">
+        <button type="submit" class="btn btn-success btn-sm text-white">
+        <i class="bi bi-box-arrow-up-right"></i> ส่งข้อความ
+        </button>
+        <button type="reset" class="btn btn-danger btn-sm ms-2">
+        <i class="bi bi-x-square"></i> ยกเลิก</button>
     </div>
-
-    <?php echo"<BR>"; ?>
-    <div style="text-align: center;">
-    <table style="border: 2px solid black; width: 40%;" align="center">
-    <form  >
-    <tr><td style="background-color: #6cd2fe;">แสดงความคิดเห็น</td></tr>
-        
-    <tr><td>
-        <textarea name='message'  row="10" cols="30">
-            The cat was playing in the garden.
-        </textarea>
-        </td></tr>
-        
-        <BR>
-        <tr><td>
-        <input type="submit" value="ส่งข้อความ"></td></tr>
-    </form>
-    </table>
     </div>
+        </form>
+</div>
+</div>
+<br>
+</div>
+<div class="col-1g-3 col-md-2 col-sm-1"></div>
+</div>
+</div>
 </body>
 </html>
--- verify.php
+++ verify.php
@@ -22,28 +22,4 @@ if($result->rowCount()==1){
     die();
 }
 $conn=null;
-?><?php include "conn.php";?>
-<?php 
-if(isset($_SESSION['id'])){
-    header("location: index.php");
-    die();
-}
-$user = $_POST['user'];
-$password = $_POST['pwd'];
-$sql = "SELECT * FROM user where user='$user' and password=sha1('$password')";
-$result = $conn->query($sql);
-if($result->rowCount()==1){
-    $data = $result->fetch(PDO:: FETCH_ASSOC);
-    $_SESSION['user'] = $data['user'];
-    $_SESSION['role'] = $data['role'];
-    $_SESSION['user_id'] = $data['id'];
-    $_SESSION['id'] = session_id();
-    header("location:index.php");
-    die();
-}else{
-    $_SESSION['error'] = "error";
-    header("location:login.php");
-    die();
-}
-$conn=null;
 ?>
